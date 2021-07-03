020019a09b354fd8a3ee77e78338f1e475fbe749
Bryan <bryangrigorie@gmail.com>
2021-07-03 14:49:55 -0400

    restructure a bit

--- to-do/models.py
+++ to-do/models.py
@@ -6,8 +6,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     pass
+
 
 class ToDO(db.Model):
     pass
