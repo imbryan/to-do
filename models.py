6a222de3242b8d53b7603e3f1bbcaae0db57bc0e
Bryan <bryangrigorie@gmail.com>
2021-07-03 15:19:57 -0400

    added one-to-many relationship between User and ToDO

--- models.py
+++ models.py
@@ -11,10 +11,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
 
+    todos = db.relationship('ToDO', backref='user', lazy=True)  # each user has multiple To-DOs
+
 
 class ToDO(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(150), nullable=False)  # the text content of a To-DO
     due_date = db.Column(db.Date)  # a user set due date for a To-DO
-    complete = db.Column(db.Boolean, nullable=False, default=False)  # completed flag for a To-Do
+    complete = db.Column(db.Boolean, nullable=False, default=False)  # completed flag for a To-DO
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # each To-DO associates with a user
 
